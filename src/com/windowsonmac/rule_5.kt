9993ff1f10f6d55136df3c4ef1b4d26a54d4cb2f
Adel Khial <adel.khial@gmail.com>
2017-11-11 23:04:23 +0000

    Finished rule 5.

---
+++ src/com/windowsonmac/rule_5.kt
@@ -0,0 +1,112 @@
+package com.windowsonmac
+
+import java.io.File
+import java.math.BigDecimal
+import java.util.ArrayList
+import java.util.HashMap
+
+/**
+ * Written for GreatUniHack 2017 by Adel Khial.
+ * Worst code ever lol, just hackathon things.
+ */
+
+fun main(args: Array<String>) {
+    val sb = StringBuilder()
+    val orders = getOrders()
+    val products = getProducts()
+    val usedProducts = arrayListOf<Product>()
+
+    val orderMap = HashMap<Int, ArrayList<Product>>()
+    var weight: BigDecimal
+    var volume: BigDecimal
+    var oldWeight: BigDecimal
+    var oldVolume: BigDecimal
+    var container = 0
+    orders.forEach {
+        if(orderMap[it.first] != null)
+            orderMap[it.first]?.add(products[it.second]!!)
+        else
+            orderMap[it.first] = arrayListOf(products[it.second]!!)
+    }
+    orderMap.forEach {
+        var sorted = it.value.sortedByDescending { it.weight/it.vol } // Same thing just sort by density
+        // for a weird approx.
+        val split = sorted.partition { it.category > 4 }
+        sorted = split.first
+        while(sorted.isNotEmpty()) {
+            container++
+            weight = BigDecimal.ZERO
+            volume = BigDecimal.ZERO
+            for(i in 0 until sorted.size) {
+                val p = sorted[i]
+                if(p.weight > 15.0 || p.vol > 65340.0) continue
+                oldVolume = volume
+                oldWeight = weight
+                volume += BigDecimal.valueOf(p.vol)
+                weight += BigDecimal.valueOf(p.weight)
+                if(weight > BigDecimal.valueOf(15.0) || volume > BigDecimal.valueOf(65340)) {
+                    volume = oldVolume
+                    weight = oldWeight
+                    continue
+                }
+                sb.append("${it.key},$container,${p.id}\n")
+                usedProducts.add(p)
+            }
+            val remaining = arrayListOf<Product>()
+            var f = false
+            for(p in sorted) {
+                for(q in usedProducts) {
+                    if(p == q) {
+                        usedProducts.remove(p)
+                        f = true
+                        break
+                    }
+                }
+                if(!f) remaining.add(p)
+                f = false
+            }
+            usedProducts.clear()
+            sorted = remaining
+        }
+        sorted = split.second
+        while(sorted.isNotEmpty()) {
+            container++
+            weight = BigDecimal.ZERO
+            volume = BigDecimal.ZERO
+            for(i in 0 until sorted.size) {
+                val p = sorted[i]
+                if(p.weight > 15.0 || p.vol > 65340.0) continue
+                oldVolume = volume
+                oldWeight = weight
+                volume += BigDecimal.valueOf(p.vol)
+                weight += BigDecimal.valueOf(p.weight)
+                if(weight > BigDecimal.valueOf(15.0) || volume > BigDecimal.valueOf(65340)) {
+                    volume = oldVolume
+                    weight = oldWeight
+                    continue
+                }
+                sb.append("${it.key},$container,${p.id}\n")
+                usedProducts.add(p)
+            }
+            val remaining = arrayListOf<Product>()
+            var f = false
+            for(p in sorted) {
+                for(q in usedProducts) {
+                    if(p == q) {
+                        usedProducts.remove(p)
+                        f = true
+                        break
+                    }
+                }
+                if(!f) remaining.add(p)
+                f = false
+            }
+            usedProducts.clear()
+            sorted = remaining
+        }
+    }
+    File("$TEAM.rule_5.csv").printWriter().use {
+        it.println("\"ORDER_ID\",\"CONTAINER_ID\",\"SKU_ID\"")
+        it.println(sb)
+    }
+}
